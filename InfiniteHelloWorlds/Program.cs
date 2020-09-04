21162845d38b7d9a04f835878805e8d2c83b3919
danebalia <dane.balia@sourcing-sa.co.za>
2020-09-04 15:34:54 +0200

    First commit

---
+++ InfiniteHelloWorlds/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace InfiniteHelloWorlds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Task.Delay(500).Wait();
+                Console.WriteLine("Hello World!");
+            }
+           
+        }
+    }
+}
